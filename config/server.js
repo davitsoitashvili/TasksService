eb91e0676922c083f73b4fa8f38404592fcb5209
Davit Soitashvili <davit.soitashvili.1@btu.edu.ge>
2021-01-17 20:55:43 +0400

    init commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1a0c499ca3337005529423943ac2a660'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '0ec18b8a-8b8d-4b49-b1ee-2976f32905fe'
+};
